a3cf94c9e7cf3fb834edeefc0d266e0f500caf1b
Filip Kudła <filip.kudla@snow.dog>
2017-10-26 23:13:45 +0200

    Fromatting in components

--- view/frontend/web/js/components/BaseProduct.vue
+++ view/frontend/web/js/components/BaseProduct.vue
@@ -1,79 +1,79 @@
 <template>
-    <div class="products" :class="containerclass">
-        <div class="products__item" :class="itemclass" v-for="product in products">
-            <p class="products__title">
-                Product name: 
-                <strong>
-                    {{ product.name }}
-                </strong>
-            </p>
+  <div class="products" :class="containerclass">
+    <div class="products__item" :class="itemclass" v-for="product in products">
+      <p class="products__title">
+        Product name:
+        <strong>
+          {{ product.name }}
+        </strong>
+      </p>
 
-            <span class="product__price">
-                Price: 
-                <strong>
-                    {{ product.price | currency }} {{ currency }}
-                </strong>
-            </span>
+      <span class="product__price">
+        Price:
+        <strong>
+          {{ product.price | currency }} {{ currency }}
+        </strong>
+      </span>
 
-            <div class="products__handler">
-                <p class="products__information">
-                    Product details:
-                </p>
+      <div class="products__handler">
+        <p class="products__information">
+          Product details:
+        </p>
 
-                <template v-for="detail in JSON.parse(product.options)">
-                    <span class="products__detail">
-                        {{ detail.label }}:
-                        <strong>
-                            {{ detail.value }}
-                        </strong>
-                    </span>
-                </template>
+        <template v-for="detail in JSON.parse(product.options)">
+          <span class="products__detail">
+            {{ detail.label }}:
+            <strong>
+              {{ detail.value }}
+            </strong>
+          </span>
+        </template>
 
-                <span class="products__detail">
-                    Qty: 
-                    <strong>
-                        {{ product.qty }}
-                    </strong>
-                </span>
+        <span class="products__detail">
+          Qty:
+          <strong>
+            {{ product.qty }}
+          </strong>
+        </span>
 
-                <template v-if="product.discount_percent > 0">
-                    <span class="product__detail">
-                        Discount: {{ product.discount_percent }}%
-                    </span>
-                </template>
-            </div>
+        <template v-if="product.discount_percent > 0">
+          <span class="product__detail">
+            Discount: {{ product.discount_percent }}%
+          </span>
+        </template>
+      </div>
 
-            <div class="action">
-                <div class="action__handler">
-                    <a href="#" class="link action__link">
-                        Go to product
-                    </a>
-                </div>
-            </div>
+      <div class="action">
+        <div class="action__handler">
+          <a href="#" class="link action__link">
+            Go to product
+          </a>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        products: {
-            type: Array
-        },
-        containerclass: {
-            type: String
-        },
-        itemclass: {
-            type: String
-        },
-        currency: {
-            type: String
-        }
+  props: {
+    products: {
+        type: Array
     },
-    filters: {
-        currency(value) {
-            return parseFloat(value).toFixed(2);
-        }
+    containerclass: {
+        type: String
+    },
+    itemclass: {
+        type: String
+    },
+    currency: {
+        type: String
     }
+  },
+  filters: {
+    currency(value) {
+        return parseFloat(value).toFixed(2);
+    }
+  }
 }
 </script>
--- view/frontend/web/js/components/steps/BaseSummaryStep.vue
+++ view/frontend/web/js/components/steps/BaseSummaryStep.vue
@@ -1,30 +1,30 @@
 <template>
-    <section v-if="step === 'summary'">
-        <h1>
-            Summary
-        </h1>
+  <section v-if="step === 'summary'">
+    <h1>
+      Summary
+    </h1>
 
-        <BaseSummary
-            :items="totals.total_segments"
-            :discountamout="totals.discount_amount"
-            :itemsqty="totals.items_qty"
-            :currencycode="totals.base_currency_code"
-        />
+    <BaseSummary
+      :items="totals.total_segments"
+      :discountamout="totals.discount_amount"
+      :itemsqty="totals.items_qty"
+      :currencycode="totals.base_currency_code"
+    />
 
-        <BaseButton
-            buttontype="button"
-            buttonclass="button"
-            text="Place order"
-            @click.native="makeOrder"
-        />
-    
-        <BaseButton
-            buttontype="button"
-            buttonclass="button"
-            text="Back"
-            @click.native="changeStep('methods')"
-        />
-    </section>
+    <BaseButton
+      buttontype="button"
+      buttonclass="button"
+      text="Place order"
+      @click.native="makeOrder"
+    />
+
+    <BaseButton
+      buttontype="button"
+      buttonclass="button"
+      text="Back"
+      @click.native="changeStep('methods')"
+    />
+  </section>
 </template>
 
 <script>
@@ -32,53 +32,53 @@ import BaseButton from '../BaseButton.vue'
 import BaseSummary from '../BaseSummary.vue'
 
 export default {
-    components: {
-        BaseButton,
-        BaseSummary,
-    },
-    data() {
-        return {
-            baseUrl: baseUrl,
-            config: this.$store.state.config
-        }
-    },
-    computed: {
-        cartId() {
-            return this.$store.state.cart;
-        },
-        step() {
-            return this.$store.state.step;
-        },
-        totals() {
-            return this.$store.state.totals;
-        },
-        selectedMethods() {
-            return this.$store.state.selectedMethods;
-        }
-    },
-    methods: {
-        changeStep(newStep) {
-            this.$store.commit('updateStep', newStep);
-        },
-        makeOrder() {
-            this.request(
-                `${this.baseUrl}index.php/rest/V1/guest-carts/${this.cartId}/order`,
-                {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        "paymentMethod": {
-                            "method": this.selectedMethods.paymentMethod.method,
-                        }
-                    })
-                }
-            )
-                .then(response => {
-                    this.$store.commit('updateStep', 'success');
-                })
-        }
+  components: {
+    BaseButton,
+    BaseSummary,
+  },
+  data() {
+    return {
+      baseUrl: baseUrl,
+      config: this.$store.state.config
     }
+  },
+  computed: {
+    cartId() {
+      return this.$store.state.cart;
+    },
+    step() {
+      return this.$store.state.step;
+    },
+    totals() {
+      return this.$store.state.totals;
+    },
+    selectedMethods() {
+      return this.$store.state.selectedMethods;
+    }
+  },
+  methods: {
+    changeStep(newStep) {
+      this.$store.commit('updateStep', newStep);
+    },
+    makeOrder() {
+      this.request(
+        `${this.baseUrl}index.php/rest/V1/guest-carts/${this.cartId}/order`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            "paymentMethod": {
+              "method": this.selectedMethods.paymentMethod.method,
+            }
+          })
+        }
+      )
+        .then(response => {
+          this.$store.commit('updateStep', 'success');
+        })
+    }
+  }
 }
 </script>
