88fe5a975a0f1149c55a31b074f799a97e6852df
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-06-22 12:48:31 +0200

    Updated vue/test-utils version

--- test/App.test.js
+++ test/App.test.js
@@ -1,11 +1,11 @@
-import { shallow } from 'vue-test-utils'
+import { shallowMount } from '@vue/test-utils'
 import App from '../view/frontend/web/js/App.vue'
 
 describe('App.test.js', () => {
   let cmp
 
   beforeEach(() => {
-    cmp = shallow(App)
+    cmp = shallowMount(App)
   })
 
   it('has the expected html structure', () => {
