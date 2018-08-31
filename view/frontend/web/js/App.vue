8a2aacf919cc2f8c5bb2dc711045c867a164c635
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-08-31 15:23:24 +0200

    Added basis styles to set 2 column layout

--- view/frontend/web/js/App.vue
+++ view/frontend/web/js/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="checkout">
-    <TheShippingStep/>
-    <ThePaymentStep/>
-    <TheSuccessStep/>
-    <ProductsList/>
-    <TotalsData/>
+    <main class="checkout__wrapper">
+      <TheShippingStep/>
+      <ThePaymentStep/>
+      <TheSuccessStep/>
+    </main>
+    <aside class="checkout__sidebar">
+      <ProductsList/>
+      <TotalsData/>
+    </aside>
   </div>
 </template>
 
@@ -27,3 +31,4 @@ export default {
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+<style src="../assets/sass/main.scss" lang="scss"></style>
