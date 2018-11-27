1257148a49d5e07cb702ef586220d2c919ea8411
Paweł Szafrański <pawel.szafranski@snow.dog>
2018-11-27 22:08:21 +0100

    #104 - updated changelog

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Snowdog_VueCheckout',
-    __DIR__
+  \Magento\Framework\Component\ComponentRegistrar::MODULE,
+  'Snowdog_VueCheckout',
+  __DIR__
 );
