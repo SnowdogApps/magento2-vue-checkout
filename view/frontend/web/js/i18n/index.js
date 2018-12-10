62a422dfc99d8956f798f4832476f2bd3d6be7e4
Filip Kudła <filip.kudla@snow.dog>
2018-12-10 16:54:48 +0100

    Add locale variable to tell our module which lang to use and translate

--- view/frontend/web/js/i18n/index.js
+++ view/frontend/web/js/i18n/index.js
@@ -5,8 +5,11 @@ import Vuei18n from 'vue-i18n'
 
 Vue.use(Vuei18n)
 
+const storeLocale = window.snowdog.storeLocale
+
 const i18n = new Vuei18n({
-  locale: 'pl', // add variable from magento with lang code
+  locale: storeLocale,
+  fallbackLocale: 'en_US', // Set as deafult lang to avoid missing translations
   messages
 })
 
