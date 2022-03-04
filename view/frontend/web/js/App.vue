7deeb8094eb727777325dbf6021de38e68763621
Paweł <pawel.szafranski@snow.dog>
2022-03-04 09:35:32 +0100

    Update vite configuration

--- view/frontend/web/js/App.vue
+++ view/frontend/web/js/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="checkout">
-    Test APP 333 11
+    Test APP
   </div>
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,39 @@
-// import ViteRestart from 'vite-plugin-restart'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-// import liveReload from 'vite-plugin-live-reload'
-// import path from 'path'
+import path from 'path'
 
 export default defineConfig({
   root: 'view/frontend/web/js',
   plugins: [
     vue()
-    // liveReload(__dirname + 'view/frontend/templates/onepage.phtml'),
-    // ViteRestart({
-    //   restart: [
-    //     'view/frontend/templates/onepage.phtml'
-    //   ]
-    // })
   ],
   base: process.env.APP_ENV === 'development'
   ? '/'
   : '/dist/',
 
-  // build: {
-  //   // output dir for production build
-  //   outDir: path.resolve(__dirname, 'view/frontend/web/js'),
-  //   emptyOutDir: true,
+  build: {
+    // output dir for production build
+    outDir: path.resolve(__dirname, 'view/frontend/web/js/dist'),
+    emptyOutDir: true,
 
-  //   // emit manifest so PHP can find the hashed files
-  //   manifest: true,
-
-  //   // esbuild target
-  //   target: 'es2018',
-
-  //   // our entry
-  //   rollupOptions: {
-  //     input: path.resolve(__dirname, 'view/frontend/web/js/index.js'),
-  //     output: {
-  //       entryFileNames: 'index.js'
-  //     }
-  //   }
-  // },
+    // emit manifest so PHP can find the hashed files
+    manifest: true,
+    target: 'es2018',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'view/frontend/web/js/index.js'),
+      output: {
+        entryFileNames: 'index.js'
+      }
+    }
+  },
   server: {
     // required to load scripts from custom host
     cors: true,
-
-    // we need a strict port to match on PHP side
-    // change freely, but update on PHP to match the same port
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+      port: 3000
+    },
     strictPort: true,
     port: 3000
   },
@@ -53,11 +43,3 @@ export default defineConfig({
     }
   }
 });
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()]
-// })
